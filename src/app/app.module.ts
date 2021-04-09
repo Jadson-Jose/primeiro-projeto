68b40c8896ed2dd0e871e0d12924623656e568d3
JadsonJose <jadsonjosesilva@gmail.com>
2021-04-09 10:22:25 -0300

    sinc

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MeuPrimeiroComponente } from './meu-primeiro/meu-primeiro-componente';
+import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MeuPrimeiroComponente,
+    MeuPrimeiro2Component
   ],
   imports: [
     BrowserModule,
---
+++ src/app/meu-primeiro/meu-primeiro-componente.ts
@@ -0,0 +1,10 @@
+import{Component} from '@angular/core';
+
+@Component({
+    selector: 'meu-primeiro-componente',
+    template: `
+        <p>Meu primeiro componente com Angular 2!</p>
+    `
+})
+
+export class MeuPrimeiroComponente { }
